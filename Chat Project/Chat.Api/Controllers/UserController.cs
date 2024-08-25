76a0dcd6c969472739d154e82bdab3b6d3e0d2d6
HusanjonDeveloper <120047912+HusanjonDeveloper@users.noreply.github.com>
2024-08-26 02:41:39 +0500

    Add GetAllUsers and GetAllUserById

--- Chat Project/Chat.Api/Controllers/UserController.cs
+++ Chat Project/Chat.Api/Controllers/UserController.cs
@@ -1,16 +1,36 @@
 
+using Chat.Api.Exceptions;
+using Chat.Api.Manager;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Chat.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UserController : ControllerBase
+    public class UserController(UserManager userManager) : ControllerBase
     {
+        private readonly UserManager _userManager = userManager;
+
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAllUsers()
         {
-            return Ok("it is working"); 
+            var users = await _userManager.GetAllUsers();
+            return Ok(users);
+        }
+        
+        
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetAllUserById(Guid id)
+        {
+            try
+            {
+                var users = await _userManager.GetAllUsers();
+                return Ok(users);
+            }
+            catch (UserNotFoundException e)
+            {
+                return BadRequest(e.Message);
+            }
         }
     }
 }
