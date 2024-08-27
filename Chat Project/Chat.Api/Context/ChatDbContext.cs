e27b1216f73c6fe99f75cd534ad9150310c215f5
HusanjonDeveloper <120047912+HusanjonDeveloper@users.noreply.github.com>
2024-08-27 20:05:32 +0500

    Add ChatDbContext

--- Chat Project/Chat.Api/Context/ChatDbContext.cs
+++ Chat Project/Chat.Api/Context/ChatDbContext.cs
@@ -1,10 +1,19 @@
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace Chat.Api.Context;
 
-public class ChatDbContext(DbContextOptions<ChatDbContext> options) : DbContext(options)
+public class ChatDbContext: DbContext
 {
+
+
+    public ChatDbContext(DbContextOptions<ChatDbContext> options) : base(options)
+    {
+
+    }
+
+
     public  DbSet<Entities.User> Users { get; set; }
     
     public DbSet<Entities.Chat> Chats { get; set; }
