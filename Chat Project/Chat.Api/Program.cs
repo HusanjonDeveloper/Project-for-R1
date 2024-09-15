5308f5a902a91f33f6181910e904aedc0cdd1f90
HusanjonDeveloper <muhammadaliyevhusanjon181@gmail.com>
2024-09-15 19:33:30 +0500

    Cloine Git

--- Chat Project/Chat.Api/Program.cs
+++ Chat Project/Chat.Api/Program.cs
@@ -13,9 +13,10 @@ namespace Chat.Api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
+            
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddScoped<IUserRepository, UserRepository>();
