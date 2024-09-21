ba748d4032914f66ab556cf5d7443a7c2ac63763
HusanjonDeveloper <muhammadaliyevhusanjon181@gmail.com>
2024-09-21 20:12:41 +0500

    Add ParseToDtoExtension Chats

--- Chat Project/Chat.Api/Extensions/ParseToDtoExtension.cs
+++ Chat Project/Chat.Api/Extensions/ParseToDtoExtension.cs
@@ -18,12 +18,25 @@ public static class ParseToDtoExtension
 
         if (users == null || users.Count == 0)
             return dtos;
-
-        foreach (var user in users)
-        {
-            dtos.Add(user.ParseToDto());
-        }
-
+        dtos.AddRange(users .Select(x => x.ParseToDto()));
         return dtos;
     }
+    
+    
+    public static ChatDto ParseToDto(this Entities.Chat chat)
+    {
+        ChatDto dto = chat.Adapt<ChatDto>();
+        return dto;
+    }
+
+    public static List<ChatDto> ParseToDtos(this List<Entities.Chat>? chats)
+    {
+        var dtos = new List<ChatDto>();
+
+        if (chats == null || chats.Count == 0)
+            return dtos;
+
+        dtos.AddRange(chats.Select(x => x.ParseToDto()));
+        return dtos;
+    } 
 }
