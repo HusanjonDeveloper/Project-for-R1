7f24a201fb44d1a5b8ad743563ef4ab0580ec5f2
HusanjonDeveloper <muhammadaliyevhusanjon181@gmail.com>
2024-09-24 17:34:11 +0500

    sdsd

--- Chat Project/Chat.Api/Repositories/IChatRepositoriy.cs
+++ Chat Project/Chat.Api/Repositories/IChatRepositoriy.cs
@@ -6,7 +6,7 @@ public interface IChatRepositoriy
     Task<List<Entities.Chat>> GetAllUserChats(Guid userId);
     Task<Entities.Chat> GetUserChatById(Guid userId,Guid chatId);
     Task<Tuple<bool, Entities.Chat?>> CheckChatExist(Guid fromUserId, Guid toUserId);
-    Task AddUserChat(Entities.Chat chat);
+    Task AddChat(Entities.Chat chat);
     Task UpdateChat(Entities.Chat chat);
     Task DeleteChat(Entities.Chat chat); 
 
