81226e4078b9569ec1f5b867c3fec4ef81c03a96
HusanjonDeveloper <muhammadaliyevhusanjon181@gmail.com>
2024-09-24 20:36:20 +0500

    Delete mighration

---
+++ Chat Project/Chat.Api/Controllers/ChatsController.cs
@@ -0,0 +1,21 @@
+using Chat.Api.Manager;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chat.Api.Controllers;
+
+[Route("api/users/{userId:guid}/[controller]")]
+[ApiController]
+
+public class ChatsController(ChatManager chatManager) : Controller
+{
+
+    private readonly ChatManager _chatManager = chatManager;
+
+    [HttpGet]
+
+    public async Task<IActionResult> GetUserChats(Guid userId)
+    {
+        var chats = await _chatManager.GetAllUserChats(userId);
+        return Ok(chats);
+    }
+}
