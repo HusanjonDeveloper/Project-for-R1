cf136af75deb97f50bdfdb0ed02562b2b93b5ba9
HusanjonDeveloper <muhammadaliyevhusanjon181@gmail.com>
2024-09-25 14:21:48 +0500

    WebAssemblyHostBuilder

--- Blazor/CustomAuthInBlazor/Program.cs
+++ Blazor/CustomAuthInBlazor/Program.cs
@@ -1,5 +1,9 @@
+using Blazored.LocalStorage;
 using CustomAuthInBlazor;
 using CustomAuthInBlazor.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
