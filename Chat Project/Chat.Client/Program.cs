103e75277d9ff08d8515acaf4a7716413ed00873
HusanjonDeveloper <muhammadaliyevhusanjon181@gmail.com>
2024-09-27 02:46:17 +0500

    lazorWebAssembly

--- Chat Project/Chat.Client/Program.cs
+++ Chat Project/Chat.Client/Program.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Components.Web;
+﻿using Chat.Client;
+using Chat.Client.Repositories;
+using Chat.Client.Repositories.Contracts;
+using Chat.Client.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Chat.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthHandler>();
+
+
+builder.Services.AddScoped<StorageService>();
+
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = 
+    new Uri("http://localhost:5068/") });
+
+
+builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddScoped<IUserIntegration, UserIntegration>();
 
 await builder.Build().RunAsync();
-
