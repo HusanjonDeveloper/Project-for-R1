3366ccd67a582a5eec824000013babbae67db9af
HusanjonDeveloper <muhammadaliyevhusanjon181@gmail.com>
2024-09-27 02:46:02 +0500

    StorageService

---
+++ Chat Project/Chat.Client/Services/StorageService.cs
@@ -0,0 +1,25 @@
+namespace Chat.Client.Services;
+
+public class StorageService(ILocalStorageService localStorageService)
+{
+    private readonly ILocalStorageService _localStorageService = localStorageService;
+
+    private const string Key = "token";
+
+    public async Task SetToken(string token)
+    {
+        await _localStorageService.SetItemAsStringAsync(key: Key, token);
+    }
+    public async Task<string?> GetToken()
+    {
+        var token =  await _localStorageService.GetItemAsync<string>(key: Key);
+
+        return token;
+    }
+
+    public static async Task DeleteToken()
+    {
+        await _localStorageService.RemoveItemAsync(Key);
+    }
+
+}
