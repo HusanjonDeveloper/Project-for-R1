c4cfa4f18458a7a7fee2db927a35e9af87d794f7
HusanjonDeveloper <muhammadaliyevhusanjon181@gmail.com>
2024-09-27 02:26:27 +0500

    AccountPages

---
+++ Chat Project/Chat.Client/Pages/AccountPages/LoginBase.razor.cs
@@ -0,0 +1,39 @@
+using System.Net;
+using Microsoft.AspNetCore.Components;
+
+public class LoginBase : ComponentBase
+{
+    [Inject]
+    IUserIntegration UserIntegration { get; set; }
+
+    [Inject]
+    NavigationManager Manager { get; set; }
+
+    [Inject]
+    ILocalStorageService StorageService { get; set; }
+
+
+    protected LoginModel Model = new();
+
+    protected async Task LoginClicked()
+    {
+
+        var (statusCode, response) = await UserIntegration.Login(Model);
+
+        bool isOk = statusCode == HttpStatusCode.OK;
+
+        bool isBadRequest = statusCode == HttpStatusCode.BadRequest;
+
+
+
+        if (isOk)
+        {
+            await StorageService.SetItemAsStringAsync("token", response);
+
+            Manager.NavigateTo($"/login-successfully/{response}");
+        }
+        else if (isBadRequest)
+            Manager.NavigateTo($"/error/{response}");
+
+
+    }
---
+++ Chat Project/Chat.Client/Repositories/Services/CustomAuthHendler.cs
@@ -0,0 +1,58 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Chat.Client.Repositories.Services;
+
+public class CustomAuthHendler :  AuthenticationStateProvider
+{
+    private readonly ILocalStorageService _localStorageService = localStorageService;
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var (userId, username, role) = await ReadJwtToken();
+        
+        var claimsPrincipal = await SetClaims(userId, username, role);
+        
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(
+            user:claimsPrincipal)));
+        
+        return await Task.FromResult(new AuthenticationState(
+            user:claimsPrincipal));
+    }
+
+    public async Task<ClaimsPrincipal> SetClaims(string? userId, string? username, string? role)
+    {
+        if(userId == null || username == null || role == null)
+            return new ClaimsPrincipal();
+        
+        var claimsPrincipal = new ClaimsPrincipal(
+            new ClaimsIdentity(
+                new List<Claim>()
+                {
+                    new Claim(ClaimTypes.NameIdentifier,userId),
+                    new Claim(ClaimTypes.Name,username),
+                    new Claim(ClaimTypes.Role,role)
+                },authenticationType:"JWTAuth"));
+        
+        return claimsPrincipal;
+    }
+
+    public async Task<Tuple<string?,string?,string?>> ReadJwtToken()
+    {
+        var token = await _localStorageService.GetItemAsync<string>("token");
+
+        if (string.IsNullOrEmpty(token))
+        {
+            return new(null,null,null);
+        }
+        
+        var security = new JwtSecurityTokenHandler();
+        var parsedToken = security.ReadJwtToken(token);
+        
+        string userId = parsedToken.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)!.Value;
+        string username = parsedToken.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name)!.Value;
+        string relo = parsedToken.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role)!.Value;
+        
+        return new (userId, username, relo);
+    }
+}
