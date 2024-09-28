84b41b2453ada92ca5365dc5ba3d4e3d9ec1ae01
HusanjonDeveloper <muhammadaliyevhusanjon181@gmail.com>
2024-09-28 15:34:41 +0500

    ContentDto

---
+++ Chat Project/Chat.Client/DTOs/ContentDto.cs
@@ -0,0 +1,13 @@
+
+public class ContentDto
+{
+    public  int Id { get; set; }
+ 
+    public  string? Caption { get; set; }
+    
+    public  string FileUrl { get; set; }
+
+    public  string? Type { get; set; }
+
+    public int MessageId { get; set; }
+}
