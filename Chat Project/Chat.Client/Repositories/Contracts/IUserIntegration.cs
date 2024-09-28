dd9379d5c92b625e8684f250f099cd2fc0e2fbd5
HusanjonDeveloper <muhammadaliyevhusanjon181@gmail.com>
2024-09-28 10:19:42 +0500

    Add UpdateBio

--- Chat Project/Chat.Client/Repositories/Contracts/IUserIntegration.cs
+++ Chat Project/Chat.Client/Repositories/Contracts/IUserIntegration.cs
@@ -9,4 +9,9 @@ public interface IUserIntegration
     Task<Tuple<HttpStatusCode, string>> Register(RegisterModel model);
 
     Task<Tuple<HttpStatusCode, object>> GetAllUsers();
+    Task<Tuple<HttpStatusCode, object?>> GetProfile(byte age);
+    
+    Task<Tuple<HttpStatusCode, object?>> UpdateAge(byte sge);
+    
+    Task<Tuple<HttpStatusCode, object?>> UpdateBio(string bio);
 }
