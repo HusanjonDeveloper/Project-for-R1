fb1a221e1fddb65d75d88e133c6c0555823de895
HusanjonDeveloper <muhammadaliyevhusanjon181@gmail.com>
2024-09-30 19:13:34 +0500

    StorageService

---
+++ Chat Project/Chat.Client/Services/StorageService.cs
@@ -0,0 +1,27 @@
+using Blazored.LocalStorage;
+
+namespace Chat.Client.Services;
+
+public class StorageService(ILocalStorageService _localStorageService)
+{
+    private static readonly ILocalStorageService _localStorageService;
+
+    private const string Key = "token";
+
+    public async Task SetToken(string token)
+    {
+        await _localStorageService.SetItemAsStringAsync(key: Key, token);
+    }
+    public async Task<string?> GetToken()
+    {
+        var token =  await _localStorageService.GetItemAsync<string>(key: Key);
+
+        return token;
+    }
+
+    public static async Task DeleteToken()
+    {
+        await _localStorageService.RemoveItemAsync(Key);
+    }
+
+}
