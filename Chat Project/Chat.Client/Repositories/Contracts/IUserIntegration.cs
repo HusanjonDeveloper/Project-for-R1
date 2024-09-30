469b6daaad57a72e20bd11bb6f8def0b3d6d6cf8
HusanjonDeveloper <muhammadaliyevhusanjon181@gmail.com>
2024-09-30 19:13:57 +0500

    IUserIntegration

---
+++ Chat Project/Chat.Client/Repositories/Contracts/IUserIntegration.cs
@@ -0,0 +1,17 @@
+using System.Net;
+using Chat.Client.Models;
+
+namespace Chat.Client.Repositories.Contracts;
+
+public interface IUserIntegration
+{
+    Task<Tuple<HttpStatusCode, string>> Login(LoginModel model);
+    Task<Tuple<HttpStatusCode, string>> Register(RegisterModel model);
+
+    Task<Tuple<HttpStatusCode, object>> GetAllUsers();
+    Task<Tuple<HttpStatusCode, object?>> GetProfile(byte age);
+    
+    Task<Tuple<HttpStatusCode, object?>> UpdateAge(byte sge);
+    
+    Task<Tuple<HttpStatusCode, object?>> UpdateBio(string bio);
+}
