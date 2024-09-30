ef9adf0625b374ada19d9dfafda1ccbde1ef1e68
HusanjonDeveloper <muhammadaliyevhusanjon181@gmail.com>
2024-09-30 18:47:58 +0500

    ChatDto

---
+++ Chat Project/Chat.Client/DTOs/ChatDto.cs
@@ -0,0 +1,12 @@
+namespace Chat.Client.DTOs;
+
+public class ChatDto
+{
+    public  Guid Id  { get; set; }
+    
+    public  List<string>? ChatNames { get; set; }
+
+    public  List<UserChatDto>? UserChats  { get; set; }
+
+    public  List<MessageDto>? Messages { get; set; }
+}
