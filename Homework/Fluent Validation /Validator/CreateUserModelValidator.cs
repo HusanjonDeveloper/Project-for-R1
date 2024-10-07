13794f4204a6beb298509a3404d3a80c20171f68
Muhammadaliyev Husanjon <120047912+HusanjonDeveloper@users.noreply.github.com>
2024-10-08 01:25:04 +0500

    Update CreateUserModelValidator.cs

--- Homework/Fluent Validation /Validator/CreateUserModelValidator.cs
+++ Homework/Fluent Validation /Validator/CreateUserModelValidator.cs
@@ -7,27 +7,27 @@ public class CreateUserModelValidator : AbstractValidator<CreateUserModel>
 {
     public CreateUserModelValidator()
     {
-        // Firstname (Ism) validatsiyasi
+        // Firstname 
         RuleFor(x => x.Firstname)
             .NotEmpty().WithMessage("Ism bo'sh bo'lishi mumkin emas")
             .MinimumLength(3).WithMessage("Ism kamida 3 ta harfdan iborat bo'lishi kerak")
             .MaximumLength(16).WithMessage("Ism 16 ta harfdan oshmasligi kerak")
             .Matches("^[A-Z]").WithMessage("Ism katta harf bilan boshlanishi kerak");
 
-        // Lastname (Familiya) validatsiyasi
+        // Lastname 
         RuleFor(x => x.Lastname)
             .NotEmpty().WithMessage("Familiya bo'sh bo'lishi mumkin emas")
             .MinimumLength(2).WithMessage("Familiya kamida 2 ta harfdan iborat bo'lishi kerak")
             .MaximumLength(16).WithMessage("Familiya 16 ta harfdan oshmasligi kerak")
             .Matches("^[A-Z]").WithMessage("Familiya katta harf bilan boshlanishi kerak");
 
-        // Username validatsiyasi
+        // Username 
         RuleFor(x => x.Username)
             .NotEmpty().WithMessage("Foydalanuvchi nomi bo'sh bo'lishi mumkin emas")
             .MinimumLength(6).WithMessage("Foydalanuvchi nomi kamida 6 ta harfdan iborat bo'lishi kerak")
             .MaximumLength(32).WithMessage("Foydalanuvchi nomi 32 ta harfdan oshmasligi kerak");
 
-        // Password validatsiyasi
+        // Password 
         RuleFor(x => x.Password)
             .NotEmpty().WithMessage("Parol bo'sh bo'lishi mumkin emas")
             .MinimumLength(8).WithMessage("Parol kamida 8 ta harfdan iborat bo'lishi kerak")
@@ -35,11 +35,11 @@ public class CreateUserModelValidator : AbstractValidator<CreateUserModel>
             .Matches("[0-9]").WithMessage("Parol kamida 1 ta raqam o'z ichiga olishi kerak")
             .Matches("[A-Za-z]").WithMessage("Parol kamida 1 ta harf o'z ichiga olishi kerak");
 
-        // Confirm Password validatsiyasi
+        // Confirm Password 
         RuleFor(x => x.ConfirmPassword)
             .Equal(x => x.Password).WithMessage("Tasdiqlash paroli asosiy parol bilan bir xil bo'lishi kerak");
 
-        // Gender (Jins) validatsiyasi
+        // Gender 
         RuleFor(x => x.Gender)
             .NotEmpty().WithMessage("Jins tanlanishi kerak")
             .Must(x => x == "Male" || x == "Female").WithMessage("Jins faqat 'Male' yoki 'Female' bo'lishi mumkin");
