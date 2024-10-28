774b749be45d1e221323694d5a81b41e6b67d611
HusanjonDeveloper <muhammadaliyevhusanjon181@gmail.com>
2024-10-28 19:17:32 +0500

    Single Responsibility Principle (SRP)

---
+++ Solid Principles Training/Solid_Principle_Training/Program.cs
@@ -0,0 +1,22 @@
+﻿
+/*
+                  # SOLID PRINCIPLE 
+
+    -- Single Responsibility Principle
+    -- Open/Closed Principle
+    -- Liskov Substitution Principle
+    -- Interface Segregation Principle
+    -- Dependency Inversion Principle
+
+     *** 
+
+              # Advantages
+
+    -- Manintainability 
+    -- Testability
+    -- Extensibility
+    -- Pralel Development
+    */
+    
+    
+    
