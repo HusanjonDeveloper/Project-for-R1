413b9617e2006ea6da817938fd3620a402249029
HusanjonDeveloper <muhammadaliyevhusanjon181@gmail.com>
2024-11-30 10:21:13 +0500

    Code rub all file

--- Chat Project/Chat.Api/Managers/MemoryCacheManager.cs
+++ Chat Project/Chat.Api/Managers/MemoryCacheManager.cs
@@ -2,24 +2,16 @@
 
 namespace Chat.Api.Managers;
 
-public class MemoryCacheManager
+public class MemoryCacheManager(IMemoryCache memoryCache)
 {
-    private readonly IMemoryCache _memoryCache;
-
-    public MemoryCacheManager(IMemoryCache memoryCache)
-    {
-        _memoryCache = memoryCache;
-    }   
-    
-    
     public  void GetOrUpdateDtos(string key, object dtos)
     {
-        _memoryCache.Set(key, dtos);
+        memoryCache.Set(key, dtos);
     }
 
     public object? GetDtos(string key)
     {
-        if (_memoryCache.TryGetValue(key, out object value))
+        if (memoryCache.TryGetValue(key, out object value))
         {
             
             return value;
